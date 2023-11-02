6fb2c69151a84fee47697b60698dcacdc2cce704
Shandea <86807815+Shandea@users.noreply.github.com>
2023-11-02 13:25:43 -0700

    movie api setup and main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      Myflix
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'bfccfa909df92e6ab6af3100753624c2'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests;
