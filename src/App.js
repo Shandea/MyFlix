d165a3e402b5b5fb2d47a2f110de8be2e3a0f7d3
Shandea <86807815+Shandea@users.noreply.github.com>
2023-11-03 09:50:51 -0700

    firebase connection

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+    </AuthContextProvider>
     </>
   );
 }
