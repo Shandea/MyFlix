c6abc796d37338bb2440a4fe7a665f13b3adc074
Shandea <86807815+Shandea@users.noreply.github.com>
2023-11-07 09:35:42 -0800

    saved shows account page add and delete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
       <Link to='/'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>
+        <h1 className='text-red-600 text-4xl font-bold font-weight-500 cursor-pointer'>
           MYFLIX
         </h1>
       </Link>
